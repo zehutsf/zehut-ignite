cbd2b81e2797e6978fde4f560b3e93f4e59ead70
Brian Kadar <brian@jelly.co>
2015-11-25 15:34:28 -0800

    Config and app metadata updates

--- src/data.js
+++ src/data.js
@@ -4,43 +4,43 @@ export default {
     {
       name: 'Vadim Ogievetsky',
       company: 'Imply',
-      body: 'We are a technology company focused on exploratory analytics with Druid',
+      body: 'We are a technology company focused on exploratory analytics with Druid.',
       image: require('../static/images/logo-imply.png')
     },
     {
       name: 'Jeremy Loya',
       company: 'WiseGuy',
-      body: 'Online marketplace for students to find and receive tutoring from the best peer tutors',
+      body: 'Online marketplace for students to find and receive tutoring from the best peer tutors.',
       image: require('../static/images/logo-wiseguy.png')
     },
     {
       name: 'Laura Athuil',
       company: 'Choux',
-      body: 'French pastry creations',
+      body: 'French pastry creations.',
       image: require('../static/images/logo-choux.png')
     },
     {
-      name: 'Jack Dreiffus',
+      name: 'Jack Dreifuss',
       company: 'Simply Legal',
-      body: 'A modern legal matter management, e-billing, accruals and timekeeper approval software.',
+      body: 'CRM for in-house legal teams.',
       image: require('../static/images/logo-simply-legal.png')
     },
     {
       name: 'Leslie Francesca',
       company: 'Leslie Francesca Designs',
-      body: 'Embracing natural elements within collections of handmade jewelry',
+      body: 'Embracing natural elements within collections of handmade jewelry.',
       image: require('../static/images/logo-francesca.png')
     },
     {
       name: 'David Pewzner',
       company: 'OnSwitch',
-      body: 'The easy and fun way to control your smart lighting',
-      image: require('../static/images/logo-onswitch.png')
+      body: 'The easy and fun way to control your smart lighting.',
+      image: require('../static/images/logo-onswitch-2.png')
     },
     {
       name: 'Benjamin Bouaziz',
       company: 'Wingpicked',
-      body: 'Crowdsourced opinions on personal fashion choices from friends and experts',
+      body: 'Crowdsourced opinions on personal fashion choices from friends and experts.',
       image: require('../static/images/logo-wing.png')
     },
     {
